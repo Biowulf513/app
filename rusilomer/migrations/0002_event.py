c1f5af964504f0628ec91260de3b5a06e0458250
Biowulf <cherepanov92@gmail.com>
2017-09-20 10:45:39 +0500

    Чистка миграций

--- rusilomer/migrations/0002_event.py
+++ rusilomer/migrations/0002_event.py
@@ -18,8 +18,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=20, verbose_name='Заголовок')),
-                ('text', models.CharField(max_length=200, verbose_name='Текст')),
-                ('event_date', models.DateTimeField(verbose_name='Дата проведения')),
+                ('text', models.TextField(max_length=200, verbose_name='Текст')),
+                ('event_date', models.DateField(verbose_name='Дата проведения')),
                 ('publication_date', models.DateTimeField(verbose_name='Дата публикации')),
                 ('location', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='rusilomer.Location', verbose_name='Место проведения')),
             ],
